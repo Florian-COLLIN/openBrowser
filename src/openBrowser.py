c938089062af20e1ba31a81f8a4aa28ef77a48d4
Florian COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-12-11 18:46:05 +0100

    Update openBrowser.py

--- src/openBrowser.py
+++ src/openBrowser.py
@@ -3,11 +3,8 @@ import sublime_plugin
 import webbrowser
 
 class OpenBrowserCommand(sublime_plugin.TextCommand):
-
     def run(self, edit, url):
-
         selection = ""
         for region in self.view.sel():
             selection += self.view.substr(region)
-
         webbrowser.open(url % selection)
